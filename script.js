95966c0ff1af6bce9537a0969b4a785e5617883d
Jake Dwyer <65734212+jdwyer6@users.noreply.github.com>
2022-02-09 13:34:56 -0600

    new changes

--- script.js
+++ script.js
@@ -5,15 +5,38 @@ var instName = ["Piccolo", "Trumpet", "Trombone", "Sax", "Tuba"];
 var numDisplay = document.querySelector(".displayNums");
 var nameDisplay = document.querySelector(".displayNames");
 
-valueChange = function(item){
+// valueChange = function(item){
 
+//     instNum.push(item.value);
+//     instName.push(instName[item.value]);
+
+//     // instList.innerHTML = instList;
+//     numDisplay.innerHTML = instNum;
+//     nameDisplay.innerHTML = instName;
+// }
+
+numDisplay.innerHTML = instNum;
+nameDisplay.innerHTML = instName;
+
+
+valueChange = function(item){
     instNum.push(item.value);
     instName.push(instName[item.value]);
-
-    // instList.innerHTML = instList;
+    
     numDisplay.innerHTML = instNum;
     nameDisplay.innerHTML = instName;
 }
 
-numDisplay.innerHTML = instNum;
+function saveInstChoice(inst){
+instName.push(inst.name);
+instNum.push(inst.value);
 nameDisplay.innerHTML = instName;
+numDisplay.innerHTML = instNum;
+return false;
+}
+
+function update(item) {
+var instAmount = item.value;
+
+document.querySelector(".instrument-amount").innerHTML = instAmount + " Piccolos";
+}
