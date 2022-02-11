a76799ff906590b7e2bbd2625cc26019ff735eb1
Jake Dwyer <65734212+jdwyer6@users.noreply.github.com>
2022-02-11 14:20:29 -0600

    work on sorting

--- script.js
+++ script.js
@@ -1,68 +1,28 @@
-var instNum = [0, 1, 5, 6, 1];
-var instName = ["Piccolo", "Trumpet", "Trombone", "Sax", "Tuba"];
-
-// var instList = document.querySelector(".instList");
-var numDisplay = document.querySelector(".displayNums");
-var nameDisplay = document.querySelector(".displayNames");
-
+var instrumentation = []; 
 var instruments = [
-{
-    name: "piccolo", 
-    voice: "soprano",
-    sort: 1
-},
-{
-    name: "flute",
-    voice: "soprano",
-    sort: 2
-},
-{
-    name: "clarinet",
-    voice: "soprano",
-    sort: 1
-}
+  {
+  name: "Piccolo",
+  instCode: 0,
+  sort: 0
+  },
+  {  
+  name: "flute",
+  instCode: 1,
+  sort: 1 
+  }
 ];
- 
-let object = {}; 
-        
-for(i=0; i< instruments.length; i++){
-   document.write(instruments[i].name + " ");
-}
-
-function addPicc(){
-object.name = "piccolo";
-object.voice = "soprano";
-object.sort = 1;
-instruments.push(object);
-}
-
-// nameDisplay.innerHTML = instruments.[i].name);
 
 
-// numDisplay.innerHTML = instNum;
-// nameDisplay.innerHTML = instName;
+let buttons = Array.from(document.getElementsByClassName("button"));
+buttons.map(button => {
+  button.addEventListener('click', (e) => {
+    instrumentation.push(button.value);
+    document.getElementById("demo").innerHTML = instrumentation;
+    var instName = instruments[button.value].name;
+    document.getElementById("names").innerHTML = instName;
+  })
+})
 
 
-// valueChange = function(item){
-//     instNum.push(item.value);
-//     instName.push(instName[item.value]);
-    
-//     numDisplay.innerHTML = instNum;
-//     nameDisplay.innerHTML = instName;
-// }
-
-// function saveInstChoice(inst){
-// instName.push(inst.name);
-// instNum.push(inst.value);
-// nameDisplay.innerHTML = instName;
-// numDisplay.innerHTML = instNum;
-// return false;
-// }
-
-function update(item) {
-var instAmount = item.value;
-
-document.querySelector(".instrument-amount").innerHTML = instAmount + " Piccolos";
-}
 
 
