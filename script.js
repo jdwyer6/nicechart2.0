64f0e27c4d7d42b196970698a81e2d9600e4f2ec
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-02-13 09:49:02 -0600

    complete sorting

--- script.js
+++ script.js
@@ -1,27 +1,111 @@
 var instrumentation = []; 
-var instruments = [
-  {
-  name: "Piccolo",
-  instCode: 0,
-  sort: 0
-  },
-  {  
-  name: "flute",
-  instCode: 1,
-  sort: 1 
-  }
-];
+var instNamesDisplay = [];
 
-
-let buttons = Array.from(document.getElementsByClassName("button"));
+let buttons = Array.from(document.getElementsByClassName("instButton"));
 buttons.map(button => {
   button.addEventListener('click', (e) => {
-    instrumentation.push(button.value);
-    document.getElementById("demo").innerHTML = instrumentation;
-    var instName = instruments[button.value].name;
-    document.getElementById("names").innerHTML = instName;
+
+    var table = document.getElementById("table");
+    for (var i=0, row; row=table.rows[i]; i++) {
+      for(var j=0, col; col=row.cells[j]; j++){
+        var inst = col.innerHTML;
+        var btn = button.name;
+      } 
+    }
+
+
+
+
+      if(e.target.id === "add"){
+        var obj = {name: button.name, sort: button.value};
+        instrumentation.push(obj);
+        console.log(instrumentation);
+        displayArray();
+
+        //Update User Display
+
+        if(inst = btn){
+          var nameToUpdate = document.getElementById(inst);
+          var quantity = parseInt(nameToUpdate.innerHTML) + 1;
+          nameToUpdate.innerHTML = quantity;
+        }
+
+        //Update User Display
+
+      } else if (e.target.id === "remove"){
+        console.log("remove");
+        //Remove Inst Value
+
+        if(inst = btn){
+          var nameToUpdate = document.getElementById(inst);
+          var quantity = 0;
+          nameToUpdate.innerHTML = quantity;
+        }
+
+
+        for( var i = 0; i < instrumentation.length; i++){ 
+    
+          if ( instrumentation[i].sort === button.value) { 
+      
+              instrumentation.splice(i, 1); 
+              i--;
+          }
+      
+      }
+      displayArray();
+       console.log(instrumentation);
+      }else{
+        console.log("no inst found");
+      }
+
   })
 })
+
+
+function compare(a, b) {
+  return a.sort - b.sort;
+}
+
+function sortInstruments() {
+instrumentation.sort(compare);
+displayArray()
+console.log(instrumentation);
+
+}
+
+
+function displayArray(){
+  // console.log(instrumentation[0].name);
+  for (let index = 0; index < instrumentation.length; index++) {
+    var element = instrumentation[index].name;
+    document.getElementById("names").innerHTML = JSON.stringify(instrumentation);
+  }
+  
+  // document.getElementById("names").innerHTML = instNamesDisplay;
+};
+
+
+//  function updateUserDisplay(){
+//   var table = document.getElementById("table");
+ 
+// for (var i=0, row; row=table.rows[i]; i++) {
+//   for(var j=0, col; col=row.cells[j]; j++){
+//     var inst = col.innerHTML;
+//     if(inst = "Piccolo"){
+//       console.log("yes");
+//     }
+   
+//   }
+      
+     
+// }
+//  }
+
+// console.log(i);
+  
+
+
+
 
 
 
