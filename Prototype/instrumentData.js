dc04b29fd69762bd660e72436ebd1205d6ef0711
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-03-23 10:20:09 -0500

    Design Fixes
    Media query for margins at smaller screen size
    copy button on right

--- Prototype/instrumentData.js
+++ Prototype/instrumentData.js
@@ -1,35 +1,227 @@
 const instruments = [
-    {name: 'Flute (Advanced)', id: 6, weight: .5},
-    {name: 'Flute', id: 11, weight: 1},
-    {name: 'Alto Flute in G', id: 16, weight: 1}
+{"name": 'Piccolo',
+"abvName": 'Picc.',
+"id": 1,
+"weight": .5,
+"common": false
+},
+{"name": 'Flute (Advanced)',
+"abvName": 'Fl. (Adv)',
+"id": 6,
+"weight": .5,
+"common": false
+},
+{
+"name": 'Flute',
+"abvName": 'Fl.',
+"id": 11,
+"weight": .3,
+"common": true
+},
+{
+"name": 'Alto Flute in G',
+"abvName": 'Alto Fl.',
+"id": 16,
+"weight": .3,
+"common": false
+},
+{
+"name": 'Oboe',
+"abvName": 'Ob.',
+"id": 21,
+"weight": .75,
+"common": true
+},
+{
+"name": 'English Horn in F',
+"abvName": 'Eng. Hn.',
+"id": 26,
+"weight": .75,
+"common": false
+},
+{
+"name": 'Bassoon',
+"abvName": 'Bsn.',
+"id": 31,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Contrabassoon',
+"abvName": 'Cbsn.',
+"id": 36,
+"weight": .3,
+"common": false
+},
+{
+"name": 'Clarinet in E♭',
+"abvName": 'E♭ Cl.',
+"id": 41,
+"weight": .5,
+"common": false
+},
+{
+"name": 'Clarinet in B♭',
+"abvName": 'B♭ Cl.',
+"id": 46,
+"weight": .5,
+"common": true
+},
+{
+"name": 'Alto Clarinet in E♭',
+"abvName": 'Alto Cl.',
+"id": 51,
+"weight": .5,
+"common": false
+},
+{
+"name": 'Bass Clarinet in B♭',
+"abvName": 'B. Cl.',
+"id": 56,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Contrabass Clarinet in B♭',
+"abvName": 'Cb. Cl.',
+"id": 61,
+"weight": 1,
+"common": false
+},
+{
+"name": 'Soprano Saxophone in B♭',
+"abvName": 'S. Sax.',
+"id": 66,
+"weight": .75,
+"common": false
+},
+{
+"name": 'Alto Saxophone in E♭',
+"abvName": 'A. Sax.',
+"id": 71,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Tenor Saxophone in B♭',
+"abvName": 'T. Sax.',
+"id": 76,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Baritone Saxophone in E♭',
+"abvName": 'B. Sax.',
+"id": 81,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Trumpet in B♭ (Advanced)',
+"abvName": 'Tpt. (Adv)',
+"id": 86,
+"weight": 1,
+"common": false
+},
+{
+"name": 'Trumpet in B♭',
+"abvName": 'Tpt.',
+"id": 91,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Horn in F',
+"abvName": 'Hn.',
+"id": 96,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Trombone',
+"abvName": 'Tbn.',
+"id": 101,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Bass Trombone',
+"abvName": 'B. Tbn.',
+"id": 106,
+"weight": 1,
+"common": false
+},
+{
+"name": 'Euphonium B.C.',
+"abvName": 'Euph. B.C.',
+"id": 111,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Euphonium T.C. in B♭',
+"abvName": 'Euph. T.C.',
+"id": 116,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Tuba',
+"abvName": 'Tba.',
+"id": 121,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Violin',
+"abvName": 'Vln.',
+"id": 126,
+"weight": .5,
+"common": true
+},
+{
+"name": 'Viola',
+"abvName": 'Vla.',
+"id": 131,
+"weight": .5,
+"common": true
+},
+{
+"name": 'Cello',
+"abvName": 'Vc.',
+"id": 136,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Contrabass',
+"abvName": 'Cb.',
+"id": 141,
+"weight": 1,
+"common": true
+},
+{
+"name": 'Percussion 1',
+"abvName": 'Perc. 1',
+"id": 146,
+"common": true
+},   
+{
+"name": 'Percussion 2',
+"abvName": 'Perc. 2',
+"id": 147,
+"common": true
+},
+{
+"name": 'Mallet Percussion',
+"abvName": 'Mllt.',
+"id": 148,
+"common": true
+},
+{
+"name": 'Timpani',
+"abvName": 'Timp.',
+"id": 149,
+"common": true
+}
 ]
-
-console.log(instruments.name[0])
-
-
-// "21">Oboe
-// "26">English Horn in F
-// "31">Bassoon
-// "36">Contrabassoon
-// "41">Clarinet in E♭
-// "46">Clarinet in B♭
-// "51">Alto Clarinet in E♭
-// "56">Bass Clarinet in B♭
-// "61">Contrabass Clarinet in B♭
-// "66">Soprano Sax in B♭
-// "71">Alto Sax in E♭
-// "76">Tenor Sax in B♭</
-// "86">Trumpet in B♭ (Advanced)
-// "91">Trumpet in B♭
-// "96">Horn in F
-// "101">Trombone
-// "106">Bass Trombone
-// "111">Euphonium B.C.
-// "116">Euphonium T.C. in B♭
-// "126">Violin
-// "131">Viola
-// "136">Cello</
-// "146">Percussion 1
-// "147">Percussion 2
-// "148">Mallet Percussion
-// "149">Timpani
--- Prototype/prototype.js
+++ Prototype/prototype.js
@@ -1,11 +1,237 @@
+const instruments = [
+    {"name": 'Piccolo',
+    "abvName": 'Picc.',
+    "id": 1,
+    "weight": .5,
+    "common": false
+    },
+    {"name": 'Flute (Advanced)',
+    "abvName": 'Fl. (Adv)',
+    "id": 6,
+    "weight": .5,
+    "common": false
+    },
+    {
+    "name": 'Flute',
+    "abvName": 'Fl.',
+    "id": 11,
+    "weight": .3,
+    "common": true
+    },
+    {
+    "name": 'Alto Flute in G',
+    "abvName": 'Alto Fl.',
+    "id": 16,
+    "weight": .3,
+    "common": false
+    },
+    {
+    "name": 'Oboe',
+    "abvName": 'Ob.',
+    "id": 21,
+    "weight": .75,
+    "common": true
+    },
+    {
+    "name": 'English Horn in F',
+    "abvName": 'Eng. Hn.',
+    "id": 26,
+    "weight": .75,
+    "common": false
+    },
+    {
+    "name": 'Bassoon',
+    "abvName": 'Bsn.',
+    "id": 31,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Contrabassoon',
+    "abvName": 'Cbsn.',
+    "id": 36,
+    "weight": .3,
+    "common": false
+    },
+    {
+    "name": 'Clarinet in E♭',
+    "abvName": 'E♭ Cl.',
+    "id": 41,
+    "weight": .5,
+    "common": false
+    },
+    {
+    "name": 'Clarinet in B♭',
+    "abvName": 'B♭ Cl.',
+    "id": 46,
+    "weight": .5,
+    "common": true
+    },
+    {
+    "name": 'Alto Clarinet in E♭',
+    "abvName": 'Alto Cl.',
+    "id": 51,
+    "weight": .5,
+    "common": false
+    },
+    {
+    "name": 'Bass Clarinet in B♭',
+    "abvName": 'B. Cl.',
+    "id": 56,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Contrabass Clarinet in B♭',
+    "abvName": 'Cb. Cl.',
+    "id": 61,
+    "weight": 1,
+    "common": false
+    },
+    {
+    "name": 'Soprano Saxophone in B♭',
+    "abvName": 'S. Sax.',
+    "id": 66,
+    "weight": .75,
+    "common": false
+    },
+    {
+    "name": 'Alto Saxophone in E♭',
+    "abvName": 'A. Sax.',
+    "id": 71,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Tenor Saxophone in B♭',
+    "abvName": 'T. Sax.',
+    "id": 76,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Baritone Saxophone in E♭',
+    "abvName": 'B. Sax.',
+    "id": 81,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Trumpet in B♭ (Advanced)',
+    "abvName": 'Tpt. (Adv)',
+    "id": 86,
+    "weight": 1,
+    "common": false
+    },
+    {
+    "name": 'Trumpet in B♭',
+    "abvName": 'Tpt.',
+    "id": 91,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Horn in F',
+    "abvName": 'Hn.',
+    "id": 96,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Trombone',
+    "abvName": 'Tbn.',
+    "id": 101,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Bass Trombone',
+    "abvName": 'B. Tbn.',
+    "id": 106,
+    "weight": 1,
+    "common": false
+    },
+    {
+    "name": 'Euphonium B.C.',
+    "abvName": 'Euph. B.C.',
+    "id": 111,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Euphonium T.C. in B♭',
+    "abvName": 'Euph. T.C.',
+    "id": 116,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Tuba',
+    "abvName": 'Tba.',
+    "id": 121,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Violin',
+    "abvName": 'Vln.',
+    "id": 126,
+    "weight": .5,
+    "common": true
+    },
+    {
+    "name": 'Viola',
+    "abvName": 'Vla.',
+    "id": 131,
+    "weight": .5,
+    "common": true
+    },
+    {
+    "name": 'Cello',
+    "abvName": 'Vc.',
+    "id": 136,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Contrabass',
+    "abvName": 'Cb.',
+    "id": 141,
+    "weight": 1,
+    "common": true
+    },
+    {
+    "name": 'Percussion 1',
+    "abvName": 'Perc. 1',
+    "id": 146,
+    "common": true
+    },   
+    {
+    "name": 'Percussion 2',
+    "abvName": 'Perc. 2',
+    "id": 147,
+    "common": true
+    },
+    {
+    "name": 'Mallet Percussion',
+    "abvName": 'Mllt.',
+    "id": 148,
+    "common": true
+    },
+    {
+    "name": 'Timpani',
+    "abvName": 'Timp.',
+    "id": 149,
+    "common": true
+    }
+    ]
+    
 const draggalbes = document.querySelectorAll('.draggable')
 const containers = document.querySelectorAll('.container')
 const rightSection = document.querySelector('.right')
 const leftSection = document.querySelector('.left')
 var instArray = []
-// var afterElement;
-// var clone;
-// var container;
+
 
 
 
@@ -236,26 +462,13 @@ button.addEventListener('click', function() {
 
 
 
-const instruments = [
-    {"name": 'Flute (Advanced)',
-    "abvName": 'Fl.(Adv)',
-    "id": 6,
-    "weight": .5,
-    "common": false
-    },
-    {
-    "name": 'Flute',
-    "abvName": 'Fl.',
-    "id": 11,
-    "weight": 1,
-    "common": true
-    }
-]
 
  function instTest(){
      for(let i=0; i<instruments.length; i++){
-         console.log(instruments[0].common)
+         console.log(instruments[12].id)
      }
  }
 
 instTest()
+
+
