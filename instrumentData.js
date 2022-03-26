d5796b3e128dbc02e5ddfded4478fc6333e4be3f
kegcheck <65734212+kegcheck@users.noreply.github.com>
2022-03-26 14:14:34 -0500

    updates

--- instrumentData.js
+++ instrumentData.js
@@ -5,223 +5,210 @@ const instruments = [
     "weight": .5,
     "common": false
     },
-    {"name": 'Flute (Advanced)',
-    "abvName": 'Fl. (Adv)',
-    "id": 6,
-    "weight": .5,
-    "common": false
-    },
     {
     "name": 'Flute',
     "abvName": 'Fl.',
-    "id": 11,
+    "id": 6,
     "weight": .3,
     "common": true
     },
     {
     "name": 'Alto Flute in G',
     "abvName": 'Alto Fl.',
-    "id": 16,
+    "id": 11,
     "weight": .3,
     "common": false
     },
     {
     "name": 'Oboe',
     "abvName": 'Ob.',
-    "id": 21,
+    "id": 16,
     "weight": .75,
     "common": true
     },
     {
     "name": 'English Horn in F',
     "abvName": 'Eng. Hn.',
-    "id": 26,
+    "id": 21,
     "weight": .75,
     "common": false
     },
     {
     "name": 'Bassoon',
     "abvName": 'Bsn.',
-    "id": 31,
+    "id": 26,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Contrabassoon',
     "abvName": 'Cbsn.',
-    "id": 36,
+    "id": 31,
     "weight": .3,
     "common": false
     },
     {
     "name": 'Clarinet in E♭',
     "abvName": 'E♭ Cl.',
-    "id": 41,
+    "id": 36,
     "weight": .5,
     "common": false
     },
     {
     "name": 'Clarinet in B♭',
     "abvName": 'B♭ Cl.',
-    "id": 46,
+    "id": 41,
     "weight": .5,
     "common": true
     },
     {
     "name": 'Alto Clarinet in E♭',
     "abvName": 'Alto Cl.',
-    "id": 51,
+    "id": 46,
     "weight": .5,
     "common": false
     },
     {
     "name": 'Bass Clarinet in B♭',
     "abvName": 'B. Cl.',
-    "id": 56,
+    "id": 51,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Contrabass Clarinet in B♭',
     "abvName": 'Cb. Cl.',
-    "id": 61,
+    "id": 56,
     "weight": 1,
     "common": false
     },
     {
     "name": 'Soprano Saxophone in B♭',
     "abvName": 'S. Sax.',
-    "id": 66,
+    "id": 61,
     "weight": .75,
     "common": false
     },
     {
     "name": 'Alto Saxophone in E♭',
     "abvName": 'A. Sax.',
-    "id": 71,
+    "id": 66,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Tenor Saxophone in B♭',
     "abvName": 'T. Sax.',
-    "id": 76,
+    "id": 71,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Baritone Saxophone in E♭',
     "abvName": 'B. Sax.',
-    "id": 81,
+    "id": 76,
     "weight": 1,
     "common": true
     },
     {
-    "name": 'Trumpet in B♭ (Advanced)',
-    "abvName": 'Tpt. (Adv)',
-    "id": 86,
-    "weight": 1,
-    "common": false
-    },
-    {
     "name": 'Trumpet in B♭',
     "abvName": 'Tpt.',
-    "id": 91,
+    "id": 81,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Horn in F',
     "abvName": 'Hn.',
-    "id": 96,
+    "id": 86,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Trombone',
     "abvName": 'Tbn.',
-    "id": 101,
+    "id": 91,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Bass Trombone',
     "abvName": 'B. Tbn.',
-    "id": 106,
+    "id": 96,
     "weight": 1,
     "common": false
     },
     {
     "name": 'Euphonium B.C.',
     "abvName": 'Euph. B.C.',
-    "id": 111,
+    "id": 101,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Euphonium T.C. in B♭',
     "abvName": 'Euph. T.C.',
-    "id": 116,
+    "id": 106,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Tuba',
     "abvName": 'Tba.',
-    "id": 121,
+    "id": 111,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Violin',
     "abvName": 'Vln.',
-    "id": 126,
+    "id": 116,
     "weight": .5,
     "common": true
     },
     {
     "name": 'Viola',
     "abvName": 'Vla.',
-    "id": 131,
+    "id": 121,
     "weight": .5,
     "common": true
     },
     {
     "name": 'Cello',
     "abvName": 'Vc.',
-    "id": 136,
+    "id": 126,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Contrabass',
     "abvName": 'Cb.',
-    "id": 141,
+    "id": 131,
     "weight": 1,
     "common": true
     },
     {
     "name": 'Percussion 1',
     "abvName": 'Perc. 1',
-    "id": 146,
+    "id": 136,
     "common": true
     },   
     {
     "name": 'Percussion 2',
     "abvName": 'Perc. 2',
-    "id": 147,
+    "id": 137,
     "common": true
     },
     {
     "name": 'Mallet Percussion',
     "abvName": 'Mllt.',
-    "id": 148,
+    "id": 138,
     "common": true
     },
     {
     "name": 'Timpani',
     "abvName": 'Timp.',
-    "id": 149,
+    "id": 139,
     "common": true
     }
     ]
